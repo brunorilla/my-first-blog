24b35dfe0c67f0eb2fecc0b360a7313aa192b5c8
developer <brunor@2hdesign.com.ar>
2017-12-18 18:00:49 -0300

    caca

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+"""def post_list(request):
+    return render(request,'blog/post_list.html', {})
+"""    
 
-def post_home(request):
-		return HttpResponse("<h1>Hello</h1>")
+def home(request):
+		return render(request,'blog/home.html')
+
+def translate(request):
+		original = request.GET['originaltext']
+		 
+		return render(request,'blog/translate.html')
+		
