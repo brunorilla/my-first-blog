f84e062cca6dcf265c4a870056d8b8e4e0e8563a
developer <brunor@2hdesign.com.ar>
2017-12-19 10:52:19 -0300

    Pig Latin translator

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.home),
+    url(r'^$', views.home, name='home'),
     url(r'^translate/', views.translate, name='translate'),
+    url(r'^about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,19 @@ def home(request):
 		return render(request,'blog/home.html')
 
 def translate(request):
-		original = request.GET['originaltext']
-		 
-		return render(request,'blog/translate.html')
+		original = request.GET['originaltext'].lower()
+		
+		translation = ''
+
+		for word in original.split():
+			if word[0] in ['a','e','i','o','u']:
+				#vowel
+				translation += word
+				translation += 'vowel'
+			else:
+				#consonant
+				translation += word
+				translation += 'consonant'		
+
+		return render(request,'blog/translate.html',{'original':original,'translation':translation})
 		
